941fa4ce8f3979820bc6a1076e1876b103c4f26f
manishsharmacodex <manishsharmadeveloper@gmail.com>
2025-03-08 01:05:25 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const number = document.getElementById("number");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click",()=>{
+    const newNumber = parseInt(number.value);
+    const result = document.getElementById("results");
+    let sum = 1;
+    for(let i = newNumber; i >= 1; i--){
+        sum = sum * i;
+    }
+    console.log(sum);
+
+    result.innerHTML = `Factorial of ${newNumber} is ${sum}`;
+})
